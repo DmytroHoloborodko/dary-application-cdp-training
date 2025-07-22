02fbb4c4342d554dd7370e35e4e4452a8955341a
Dmytro Holoborodko <dmytro.holoborodko@gmail.com>
2025-07-22 19:34:35 +0200

    Add a workflow file

---
+++ lambda_layer/data_models.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from typing import List, Optional, Union
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+
+class PostBase(BaseModel):
+    title: str = Field(..., max_length=200)
+    body: str = Field(..., max_length=2000)
+    tags: Optional[List[str]] = []
+
+
+class Post(PostBase):
+    id: UUID
+    createdDate: datetime
+    updatedDate: datetime
+
+
+class Response(BaseModel):
+    statusCode: int
+    body: str
+    headers: dict = {
+        "Content-Type": "application/json"
+    }
