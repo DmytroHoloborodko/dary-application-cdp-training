1d0095d92d88f0a28db9cbf952dc8b92e61e7353
Dmytro Holoborodko <dmytro.holoborodko@gmail.com>
2025-07-24 22:16:47 +0200

    Finish the POST handler

--- lambda_functions/create_post.py
+++ lambda_functions/create_post.py
@@ -1,5 +1,6 @@
 import json
 
+from data_models import Response, Post
 from dynamodb_client import get_table
 from utils import generate_post_id, now_utc
 
@@ -7,57 +8,29 @@ table = get_table()
 
 
 def lambda_handler(event, context):
-    print("" + str(event))
-    print(context)
     try:
-
-        title = event.get("title")
-        content = event.get("body")
-        tags = event.get("tags", [])
-
-        if not title or not content or not tags:
-            return {
-                "statusCode": 400,
-                "body": json.dumps({"error": "Missing at least one of required fields: title, body or tags"})
-            }
-
-        if not isinstance(tags, list):
-            return {
-                "statusCode": 400,
-                "body": json.dumps({"error": "Tags must be a list of strings"})
-            }
-
-        # Generate post metadata
-        post_id = generate_post_id()
         now = now_utc()
+        post = Post(**json.loads(event['body']), id=generate_post_id(), createdDate=now, updatedDate=now)
 
-        item = {
-            "id": post_id,
-            "title": title,
-            "body": content,
-            "tags": tags,
-            "createdDate": now,
-            "updatedDate": now
-        }
+        table.put_item(Item=post.model_dump(mode="json"))
 
-        # Save to DynamoDB
-        table.put_item(Item=item)
-        print(item)
-
-        return item
+        return Response(
+            statusCode=201,
+            body=json.dumps(post.model_dump(mode="json"))
+        ).model_dump()
 
     except Exception as e:
-        return {
-            "statusCode": 500,
-            "body": json.dumps({"error": str(e)})
-        }
+        return Response(
+            statusCode=500,
+            body=str(e)
+        ).model_dump()
 
 
 if __name__ == "__main__":
-    # test how it runs on aws
-    event = {
-        "title": "Test title",
-        "body": "Test body",
-        "tags": ["test", "tag"]
+    test_event = {
+        'resource': '/posts',
+        'path': '/posts',
+        'httpMethod': 'POST',
+        'body': '{\r\n    "title": "Test title",\r\n    "body": "Test body",\r\n    "tags": [\r\n        "test",\r\n        "tag"\r\n    ]\r\n}'
     }
-    lambda_handler(event, None)
+    print(lambda_handler(test_event, None))
